13b04afb07d54e193967e0883d4009cee44c9b42
AlkhazovAvdalim <avdalim@outlook.com>
2023-11-28 09:29:02 +0100

    Solved Ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array_, ...itemstoremove) {
+    console.log(array_)
+    console.log(itemstoremove)
+    for (let i=0;i<itemstoremove.length;i++){
+        let index = array_.indexOf(itemstoremove[i])
+        let x = array_.splice(index,1)
+    }
+    return (array_)
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
