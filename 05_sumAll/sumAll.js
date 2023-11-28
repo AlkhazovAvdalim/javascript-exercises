4d8b3964ea0dd47d2d29322997360d1a22ad5a91
AlkhazovAvdalim <avdalim@outlook.com>
2023-11-28 09:33:40 +0100

    Solved Ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start_,end_) {
+    let sum = 0
+    for (let i=start_;i<end_+1;i++){
+        console.log(i)
+         sum = sum + i
+    }
+    return (sum)
 };
 
 // Do not edit below this line
