d48d4aa2fc2a55eac2671d04bfad5abe6e2a7520
AlkhazovAvdalim <avdalim@outlook.com>
2023-11-28 09:04:58 +0100

    Solved Ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let total = ""
+    for (let i=1; i<num+1; i++){
+        total += string
+    }
+    console.log(total)
+    return total
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+repeatString("hey",4)
