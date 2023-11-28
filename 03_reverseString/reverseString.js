670694bf3f79b7af2309460957eaa938e4029588
AlkhazovAvdalim <avdalim@outlook.com>
2023-11-28 09:13:57 +0100

    Solved Ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    console.log(string)
+    string = string.split('')
+    console.log(string)
+    string = string.reverse()
+    console.log(string)
+    string = string.join('')
+    console.log(string)
+    return(string)
 };
 
 // Do not edit below this line
